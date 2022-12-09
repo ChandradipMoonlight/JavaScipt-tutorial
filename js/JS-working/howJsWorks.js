9d6d6f8b34ba966014aec306548d5293a269c594
ChandradipMoonlight <chandradipshivankar@gmail.com>
2022-12-09 21:57:24 +0530

    Class and method completed

---
+++ js/JS-working/howJsWorks.js
@@ -0,0 +1,30 @@
+// compilation
+
+// code execution
+
+// why compilation
+
+// how js code execute ?
+
+// what is global execution context?
+
+// what is local execution context?
+
+// closures?
+
+// console.log(this);
+// console.log(window);
+// console.log(firstName);
+// console.log(myFunction);
+
+// function myFunction() {
+//     console.log("This is my Function.");
+// }
+
+// var firstName = "Chandradip";
+// console.log(firstName);
+
+// console.log(firstName);
+// var firstName;
+// console.log(firstName);
+let foo = "foo";
---
+++ js/closures/closure1.js
@@ -0,0 +1,66 @@
+/**
+ * Closure
+ * 
+ * function can return function
+ */
+
+
+// function printFullName(firstName, lastName) {
+//     function printName() {
+//         console.log(firstName, lastName);
+//     }
+//     return printName;
+// }
+
+// const ans = printFullName("Chandradip", "Shivankar");
+
+// ans();
+
+function myFunction(power) {
+    return function(number) {
+        return number ** power;
+    }
+}
+
+const power = myFunction(2);
+
+console.log(power(3))
+
+const getKube = power => number => number ** power;
+
+// console.log(getKube(3)(2));
+const cube = getKube(3);
+console.log(cube(2));
+
+function func() {
+    let count = 0;
+    let exe = () => {
+        if (count < 1) {
+            console.log("Hi, You called me.");
+            count++;
+        } else {
+            console.log("Already called me.");
+        }
+    }
+    return exe;
+
+}
+
+let funcImp = () => {
+    let count = 0;
+    return () => {
+        count < 1 ? console.log("Hi, you called me.") : console.log("Already was called.");
+        count++;
+    }
+}
+
+// const myFunc = func();
+// myFunc();
+// myFunc();
+
+// const myFunc1 = func();
+// myFunc1();
+
+const result = funcImp();
+result();
+result();
